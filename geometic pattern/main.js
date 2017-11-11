c00d5c3e36b16566f3daf81a43ad449295c6a4ef
flagthefootball <melaniejones760@gmail.com>
2017-11-11 04:19:35 +0000

    message

--- geometic pattern/main.js
+++ geometic pattern/main.js
@@ -1,22 +1,36 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
+var circleRadius;
 
 function setup() {
   createCanvas(480, 600);
   circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
 }
 
 function draw() {
-  var y = 0;
-  while (y <= height) {
+  var isShifted = false;
 
-    var x = 0;
+  var y = height;
+  while (y >= 0) {
+
+    var x;
+
+    if (isShifted) {
+      x = circleRadius;
+    } else {
+      x = 0;
+    }
+
     while (x <= width) {
+      fill(color(169, 195, 205));
+      stroke(color(0, 180, 290));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
-    y = y + circleDiameter;
+    y = y - circleRadius;
+    isShifted = !isShifted;
   }
   ellipse(0, height/2, circleDiameter, circleDiameter);
   ellipse(circleDiameter, height/2, circleDiameter, circleDiameter);
