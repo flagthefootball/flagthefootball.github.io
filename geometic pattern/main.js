9f431dfe78794dda3c50c60a240da8014efa4b63
flagthefootball <melaniejones760@gmail.com>
2017-12-16 02:38:23 +0000

    Create Dodge Game

--- geometic pattern/main.js
+++ geometic pattern/main.js
@@ -1,11 +1,23 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
 
+var rVal;
+var gVal;
+var bVal;
+
 function setup() {
   createCanvas(480, 600);
+
+  frameRate(5);
+
   circleDiameter = width/NUM_CIRCLES;
   circleRadius = circleDiameter/2;
+
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 }
 
 function draw() {
@@ -23,27 +35,17 @@ function draw() {
     }
 
     while (x <= width) {
-      fill(color(169, 195, 205));
-      stroke(color(0, 180, 290));
+      stroke(color(rVal, gVal, bVal));
+      fill(color(rVal, gVal, bVal));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
     y = y - circleRadius;
     isShifted = !isShifted;
+
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
   }
-  ellipse(0, height/2, circleDiameter, circleDiameter);
-  ellipse(circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(2*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(3*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(4*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(5*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(6*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(7*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(8*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(9*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(10*circleDiameter, height/2, circleDiameter, circleDiameter);
-  ellipse(11*circleDiameter, height/2, circleDiameter, circleDiameter);   
-  ellipse(12*circleDiameter, height/2, circleDiameter, circleDiameter);
-  
 }
